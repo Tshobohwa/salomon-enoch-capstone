ca65f13b6f1cb54e575bca959a53c3f588dfeacc
Salomon Tshobohwa <stshobohwa@gmail.com>
2023-03-24 10:40:49 +0200

    add likes feature

--- src/modules/shows.js
+++ src/modules/shows.js
@@ -1,7 +1,41 @@
-import { renderShow, showPopupShow } from './DOM.js';
+import { renderShow, showPopupShow } from "./DOM.js";
 
-export const showsAPIUrl = 'https://api.tvmaze.com/shows';
+const showsAPIUrl = "https://api.tvmaze.com/shows";
 
+const involvementAPIUrl =
+  "https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/";
+
+const getShowLikes = async () => {
+  const response = await fetch(
+    `${involvementAPIUrl}apps/WzsfxsnhHGLs9rSilbgJ/likes/`
+  );
+  const responseData = await response.text();
+  const data = responseData && JSON.parse(responseData);
+  console.log(data);
+  return data;
+};
+
+const likeShow = async (showId) => {
+  const response = fetch(
+    `${involvementAPIUrl}apps/WzsfxsnhHGLs9rSilbgJ/likes/`,
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        item_id: showId,
+      }),
+    }
+  );
+  const data = await response.json();
+  console.log(data);
+  return data;
+};
+
+likeShow(12);
+
+getShowLikes();
 export const getShows = async () => {
   const response = await fetch(showsAPIUrl);
   if (!response.ok) return;
@@ -16,8 +50,6 @@ export const getOneShow = async (showId) => {
   if (!response.ok) return;
   const show = await response.json();
   showPopupShow(show);
-  // eslint-disable-next-line consistent-return
-  return show;
 };
 
 getShows();
