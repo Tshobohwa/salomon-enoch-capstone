6978803d545a5355d8630dd24a1614342e785b37
Salomon Tshobohwa <stshobohwa@gmail.com>
2023-03-25 11:34:56 +0200

    Fix likes

---
+++ src/modules/likes.js
@@ -0,0 +1,20 @@
+import { renderShow } from "./DOM.js";
+// import { getOneShow } from './shows.js';
+
+export const getshowLikes = async () => {
+  const response = await fetch(
+    "https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/WzsfxsnhHGLs9rSilbgJ/likes/"
+  );
+  const data = await response.json();
+  return data;
+};
+
+getshowLikes();
+
+export const getOneShowLikes = async (show) => {
+  const showId = show.id;
+  const allShowsLikes = await getshowLikes();
+  show.likes =
+    allShowsLikes.find((show) => show.item_id === showId)?.likes || 0;
+  renderShow(show);
+};
